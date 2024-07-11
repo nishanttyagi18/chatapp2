873f179533c9ab01ba4118824308d93bad9f48c5
nishanttyagi18 <ntyagi893@gmail.com>
2024-07-11 16:02:18 +0530

    second commit

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket(`ws://${window.location.host}`);
+const ws = new WebSocket(`wss://${window.location.host}`);
 const messages = document.getElementById('messages');
 const messageInput = document.getElementById('messageInput');
 const sendButton = document.getElementById('sendButton');
